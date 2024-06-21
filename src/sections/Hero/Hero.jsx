8dc9e7cd4e8bf8a64d8ac84a384a52a49f3e85e0
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-06-21 15:16:50 +0900

    Social media icons added

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
 import themeIcon from '../../assets/sun.svg'
+import twitterIcon from '../../assets/twitter-light.svg'
+import githubIcon from '../../assets/github-light.svg'
+import linkedinIcon from '../../assets/linkedin-light.svg'
+
 
 const Hero = () => {
   return (
@@ -15,6 +19,21 @@ const Hero = () => {
             src={themeIcon} 
             alt='Color mode' 
             className={styles.colorMode}/>
+        </div>
+        <div className={styles.info}>
+          <h1>Alberto <br/>Rosell</h1>
+          <h2>Frontend Developer</h2>
+          <span>
+            <a href='https:/twitter.com/' target='_blank'>
+              <img src={twitterIcon} alt='Twitter Icon'/>
+            </a>
+            <a href='https:/github.com/' target='_blank'>
+              <img src={githubIcon} alt='Github Icon'/>
+            </a>
+            <a href='https:/linkedin.com/' target='_blank'>
+              <img src={linkedinIcon} alt='Linkedin Icon'/>
+            </a>
+          </span>
         </div>
       </section>
       
