080c9ef7f99cd255fd1d4202e82963940a03fa01
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-06-26 19:42:04 +0900

    Hero section finished

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,15 +1,22 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
-import themeIcon from '../../assets/sun.svg'
-import twitterIcon from '../../assets/twitter-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
+import githubLight from '../../assets/github-light.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedinLight from '../../assets/linkedin-light.svg'
+import linkedinDark from '../../assets/linkedin-dark.svg'
 import CV from '../../assets/cv.pdf'
 import { useTheme } from '../../common/ThemeContext'
 
 
 const Hero = () => {
-  const {theme, toggleTheme} = useTheme;
+  const {theme, toggleTheme} = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light'? linkedinLight : linkedinDark;
+
   return (
     <div>
       <section id='hero' className={styles.container}>
@@ -20,8 +27,9 @@ const Hero = () => {
             alt='profile picture'/>
           <img 
             src={themeIcon} 
-            alt='Color mode' 
-            className={styles.colorMode}/>
+            alt='Color mode icon' 
+            className={styles.colorMode}
+            onClick={toggleTheme}/>
         </div>
         <div className={styles.info}>
           <h1>Alberto <br/>Rosell</h1>
@@ -34,7 +42,7 @@ const Hero = () => {
               <img src={linkedinIcon} alt='Linkedin Icon'/>
             </a>
           </span>
-          <p>With a passion for developing modern React web apps for any kind of busimesses</p>
+          <p className={styles.description}>With a passion for developing modern React web apps for any kind of busimesses</p>
           <a href={CV} download>
             <button className='hover'>Resume</button>
           </a>
