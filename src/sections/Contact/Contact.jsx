6f041a611e466250979efa85a68831ee400d52f4
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-06-28 19:05:08 +0900

    Styling contact section

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,6 +4,21 @@ const Contact = () => {
   return (
     <section id='contact' className={styles.container}>
         <h1 className='sectionTitle'>Contact me</h1>
+        <form action=''>
+            <div className='formGroup'>
+                <label htmlFor='name' hidden>Name</label>
+                <input type='text' id='name' name='name' placeholder='Your Name' required/>
+            </div>
+            <div className='formGroup'>
+                <label htmlFor='email' hidden>Email</label>
+                <input type='email' id='email' name='email' placeholder='Your Email' required/>
+            </div>
+            <div className='formGroup'>
+                <label htmlFor='message' hidden>Message</label>
+                <textarea id='message' name='message' placeholder='Your Message' required></textarea>
+            </div>
+            <input className='hover btn' type='submit' value='Submit'/>
+        </form>
 
 
     </section>
